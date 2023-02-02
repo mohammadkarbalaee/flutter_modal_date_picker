97cf050de861e4b59dbb335b2d3b6b21cc82add1
Muhammad Karbalaee <muhammad.ksht@gmail.com>
2023-02-03 01:37:41 +0330

    added the example project and version 0.0.2

---
+++ example/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:flutter_modal_date_picker/flutter_modal_date_picker.dart';
+
+void main() {
+  runApp(MaterialApp(theme: ThemeData(), home: MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    
+    SchedulerBinding.instance.addPostFrameCallback((timeStamp) {
+      CustomDatePickerDialog(context, DateTime.now());
+    });
+
+    return Scaffold(
+      backgroundColor: Colors.white,
+    );
+  }
+}
