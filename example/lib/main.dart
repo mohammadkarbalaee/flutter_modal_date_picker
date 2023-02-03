0b53641a7dccf88ad6c5ef84dc7c5f3bd76984bf
Muhammad Karbalaee <muhammad.ksht@gmail.com>
2023-02-03 13:47:09 +0330

    example for v0.1.0 is ready

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -6,18 +6,53 @@ void main() {
   runApp(MaterialApp(theme: ThemeData(), home: MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    
-    SchedulerBinding.instance.addPostFrameCallback((timeStamp) {
-      showModalDatePicker(context, DateTime.now());
-    });
+  State<MyApp> createState() => _MyAppState();
+}
 
-    return Scaffold(
-      backgroundColor: Colors.white,
+class _MyAppState extends State<MyApp> {
+  String date = "";
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+          backgroundColor: Colors.white,
+          body: Container(
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    child: Text(
+                      'show date picker',
+                      style: TextStyle(fontSize: 30),
+                    ),
+                    onPressed: () async {
+                      String chosenDate =
+                          await showModalDatePicker(context, DateTime.now());
+                      setState(() {
+                        date = chosenDate;
+                      });
+                    },
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                    date,
+                    style: TextStyle(
+                      backgroundColor: Colors.amber,
+                      fontSize: 30
+                    ),
+                  )
+                ],
+              ),
+            ),
+          )),
     );
   }
 }
